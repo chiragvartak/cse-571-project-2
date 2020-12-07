ec7858ba39c75d4c482782caa257012ad13c25fe
Neil Franks <neilmichaelfranks@gmail.com>
2020-12-06 19:44:23 -0700

    layoutDuplicator.py will save every flip and transpose of the layout you give it. Use this to train on more diverse layouts

---
+++ multiagent/layoutDuplicator.py
@@ -0,0 +1,49 @@
+import sys, types, time, random, os
+
+def transpose(layoutName):
+    file = "layouts/%s.lay" % layoutName
+    transposeFile = "layouts/%sT.lay" % layoutName
+    with open(file, 'r') as f:
+        array = f.read()
+
+    rows = array.split("\n")
+    with open(transposeFile, 'w') as f:
+        for j in range(len(rows[0])):
+            for row in rows:
+                if row != '':
+                    f.write(row[j])
+            if j != len(rows[0])-1:
+                f.write("\n")
+
+    return "%sT" % layoutName
+
+def flip(layoutName):
+    file = "layouts/%s.lay" % layoutName
+    flipFile = "layouts/%sF.lay" % layoutName
+    with open(file, 'r') as f:
+        array = f.read()
+
+    rows = array.split("\n")
+    with open(flipFile, 'w') as f:
+        for i in range(len(rows)):
+            row = rows[i]
+            for j in range(len(rows[0])-1,-1,-1):
+                if row != '':
+                    f.write(row[j])
+            if i != len(rows)-1:
+                f.write("\n")
+
+    return "%sF" % layoutName
+
+def generate(layoutName):
+    layoutT = transpose(layoutName)
+    layoutF = flip(layoutName)
+    layoutTF = flip(layoutT)
+    layoutFT = transpose(layoutF)
+    layoutFTF = flip(layoutFT)
+    layoutTFT = transpose(layoutTF)
+    layoutTFTF = flip(layoutTFT)
+
+if __name__ == '__main__':
+    args = sys.argv[1:]
+    generate(args[0])
